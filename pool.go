0289b3e61fca62cb05eb1c2355d6fb943d8f4853
AlexNg <contact@ngjx.org>
2025-07-23 16:16:49 +0800

    feat: Add LogMessage pool
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ pool.go
@@ -0,0 +1,40 @@
+package log
+
+import "sync"
+
+// recycled log messages to reduce allocations
+var logMsgPool = sync.Pool{
+	New: func() any {
+		return &LogMessage{
+			Meta: make([]LogMessageMetaKV, 0, 1),
+		}
+	},
+}
+
+func acquireLogMessage(msg *LogMessage) *LogMessage {
+	lm := logMsgPool.Get().(*LogMessage)
+
+	lm.Timestamp = msg.Timestamp
+	lm.Level = msg.Level
+	lm.Message = msg.Message
+	lm.trace = msg.trace
+	lm.caller = msg.caller
+
+	lm.Meta = lm.Meta[:0]
+	lm.Meta = append(lm.Meta, msg.Meta...)
+
+	return lm
+}
+
+func releaseLogMessage(lm *LogMessage) {
+	lm.Meta = lm.Meta[:0]
+	lm.trace = ""
+	lm.caller = ""
+
+	// shrink if overinflated
+	if cap(lm.Meta) > 16 {
+		lm.Meta = make([]LogMessageMetaKV, 0, 1)
+	}
+
+	logMsgPool.Put(lm)
+}
