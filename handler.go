f80dcf2484f2d11ae7d03fb2c114749f99217313
AlexNg <contact@ngjx.org>
2025-07-23 16:17:44 +0800

    feat: Add handlers
    Signed-off-by: AlexNg <contact@ngjx.org>

---
+++ handler.go
@@ -0,0 +1,113 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+)
+
+// Loggers use LogHandlers under the hood
+// to handle log messages
+type LogHandler interface {
+	Handle(loggerName string, msg *LogMessage)           // handles the log message
+	FormatLog(loggerName string, msg *LogMessage) string // formats the log message
+
+	Start() error    // starts the handler
+	Close() error    // closes the handler
+	IsRunning() bool // returns true if the handler is running
+}
+
+type BaseHandler struct {
+	LogHandler
+}
+
+func (b *BaseHandler) FormatLog(loggerName string, msg *LogMessage) string {
+	return msg.String(loggerName)
+}
+
+type WriterHandler struct {
+	BaseHandler
+
+	wg      sync.WaitGroup
+	mu      sync.RWMutex
+	logCh   chan string
+	closeCh chan struct{}
+
+	writer    io.Writer
+	cleanupId uint64
+}
+
+func (w *WriterHandler) Handle(loggerName string, msg *LogMessage) {
+	if !w.IsRunning() {
+		return
+	}
+
+	select {
+	case w.logCh <- w.FormatLog(loggerName, msg):
+	default: // drop
+	}
+}
+
+func (w *WriterHandler) Start() error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.logCh != nil {
+		return ErrAlreadyStarted
+	}
+	if w.writer == nil {
+		w.writer = io.Discard
+	}
+
+	w.logCh = make(chan string, 1<<10)
+	w.closeCh = make(chan struct{})
+	w.cleanupId = registerCleanup(func() error { w.Close(); return nil })
+
+	w.wg.Add(1)
+	go noPanicReRunVoid("log-handler", w.logWriter)
+
+	return nil
+}
+
+func (w *WriterHandler) Close() error {
+	if !w.IsRunning() {
+		return ErrNotStarted
+	}
+
+	w.mu.Lock()
+	defer w.mu.Unlock()
+
+	if w.writer != os.Stdout && w.writer != os.Stderr {
+		if closer, ok := w.writer.(io.Closer); ok {
+			closer.Close()
+		}
+	}
+
+	unregisterCleanup(w.cleanupId)
+	w.cleanupId = 0
+
+	close(w.closeCh)
+	w.logCh = nil
+	w.wg.Wait()
+
+	return nil
+}
+
+func (i *WriterHandler) IsRunning() bool {
+	i.mu.RLock()
+	defer i.mu.RUnlock()
+	return i.logCh != nil
+}
+
+func (w *WriterHandler) logWriter() {
+	for {
+		select {
+		case line := <-w.logCh:
+			fmt.Fprint(w.writer, line)
+		case <-w.closeCh:
+			w.wg.Done()
+			return
+		}
+	}
+}
